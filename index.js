0e3910b634e259356ff64970daceac955367e079
Rafael Valentim <rmoraesvalentim@gmail.com>
2019-04-27 15:38:02 -0300

    feat: add Watson Natural LAnguage Understanding service

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ const robots = {
 }
 
 async function start() {
-    const content = {};
+    const content = {
+        maximumSentences: 7
+    };
 
     content.searchTerm = askAndReturnSearchTerm();
     content.prefix = askAndReturnPrefix();
 
     await robots.text(content);
-
-    console.log(content);
 
     function askAndReturnSearchTerm() {
         return readLine.question('Type Wikipedia search term: ');
@@ -24,8 +24,8 @@ async function start() {
 
         return selectedPrefixText;
     }
-
-    //console.log(content);
+    
+    console.log(JSON.stringify(content, null, 4));
 }
 
 start();
